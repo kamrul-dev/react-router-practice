abc9f4b1309b5f74d4652cc73a6c032ebb0b3761
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-29 15:00:44 +0600

    data load for user

---
+++ src/components/User/User.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const User = () => {
+    return (
+        <div>
+            <h2>This is user.</h2>
+        </div>
+    );
+};
+
+export default User;
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
+    console.log(users);
+    useEffect(() => {
+        const url = `https://jsonplaceholder.typicode.com/users`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setUsers(data));
+    }, []);
     return (
         <div>
-            <h2>This is users page.</h2>
+            <h2>This is users list page.</h2>
         </div>
     );
 };
