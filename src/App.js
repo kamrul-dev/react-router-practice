eeda98e88070da4ade00527e6edaba2c2c03fc57
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-29 12:26:00 +0600

    Routes and route declared

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Users from './components/Users/Users';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/users" element={<Users></Users>}>Users</Route>
+        <Route path="/about" element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
